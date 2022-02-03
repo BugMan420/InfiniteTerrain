0ac4db29787c5b6170d66279359c52d348b2913b
BugMan420 <ismaelsim@hotmail.co.uk>
2022-02-03 10:46:57 +0000

    First commit of terrain simulator

---
+++ src/main/java/com/Izzy/TerrainSim/Main.java
@@ -0,0 +1,4 @@
+package com.Izzy.TerrainSim;
+
+public class Main {
+}
---
+++ src/main/java/com/Izzy/TerrainSim/Player.java
@@ -0,0 +1,7 @@
+package com.Izzy.TerrainSim;
+
+public class Player {
+    public static int maxHp;
+    public static int currentHp;
+
+}
